852ed54cd27d55c5124c9c7301c398771b1f817d
Lucas Grossi <101760538+LgrossiC@users.noreply.github.com>
2022-12-01 20:40:04 -0300

    Add files via upload

---
+++ Interface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Interface
+{
+    // interface
+    public interface xyz
+    {
+        void metodoA();
+        void metodoB();
+    }
+
+    // implementação de interface
+    class testeInterface : xyz
+    {
+        public void metodoA()
+        {
+            Console.WriteLine("metodoA");
+        }
+        public void metodoB()
+        {
+            Console.WriteLine("metodoB");
+        }
+    }
+    class interfaceDemo
+    {
+        static void Main(string[] args)
+        {
+            testeInterface obj = new testeInterface();
+            obj.metodoA();
+            obj.metodoB();
+        }
+    }
+}
---
+++ Polimorfismo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Polimorfismo
+{
+    public abstract class Empregado
+    {
+        public virtual void nomeLider()
+        {
+        }
+    }
+
+    public class rhDepartamento : Empregado
+    {
+        public override void nomeLider()
+        {
+            Console.WriteLine("AAA");
+        }
+    }
+    public class tiDepartamento : Empregado
+    {
+        public override void nomeLider()
+        {
+            Console.WriteLine("BBB");
+        }
+    }
+
+    public class finDepartamento : Empregado
+    {
+        public override void nomeLider()
+        {
+            Console.WriteLine("CCC");
+        }
+    }
+
+    class PolymorphismDemo
+    {
+        static void Main(string[] args)
+        {
+            rhDepartamento obj1 = new rhDepartamento();
+            tiDepartamento obj2 = new tiDepartamento();
+            finDepartamento obj3 = new finDepartamento();
+
+            obj1.nomeLider();
+            obj2.nomeLider();
+            obj3.nomeLider();
+
+            Console.ReadKey();
+        }
+    }
+}
